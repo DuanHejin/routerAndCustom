dd7e436d73134619b36ab7c53d66f3540e11a438
duanhejin <https://github.com/DuanHejin/router>
2018-04-18 00:45:40 +0900

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {StockComponent} from './stock/stock.component';
+import {Code404Component} from './code404/code404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'aa', redirectTo: 'home', pathMatch: 'prefix'},
+  {path: 'home', component: HomeComponent},
+  {path: 'stock/:id', component: StockComponent, data: [{testValue: "aa"}]},
+  {path: '**', component: Code404Component},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { StockComponent } from './stock/stock.component';
+import { Code404Component } from './code404/code404.component';
+import { OnlyNumberDirective } from './only-number.directive';
+
+import {EditorModule} from 'primeng/editor';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StockComponent,
+    Code404Component,
+    OnlyNumberDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    EditorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
